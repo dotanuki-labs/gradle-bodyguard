3c0ec827194226a60bd4ef0f9b6a75bf400477af
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-06-01 06:34:16 +0200

    Added fetcher for OSSIndex

---
+++ gradle_bodyguard/ossindex_fetcher.py
@@ -0,0 +1,28 @@
+import requests
+
+OSS_INDEX_URL='https://ossindex.sonatype.org/api/v3/component-report'
+DEFAULT_SECONDS_TO_TIMEOUT = 10
+
+class OSSIndexFetcher:
+	def __init__(self, url=OSS_INDEX_URL, timeout=DEFAULT_SECONDS_TO_TIMEOUT):
+		self.url = url
+
+	def fetch(self, coordinates):
+		founded = {}
+
+		response = requests.post(self.url, json={'coordinates': coordinates})
+
+		if response.status_code == requests.codes.ok:	
+			for item in response.json():
+				for vulnerability in item['vulnerabilities']:
+					founded[item['coordinates']] = vulnerability['cve']
+		else :
+			self.report_http_error(response)
+		
+		return founded
+
+	def report_http_error(self, response):
+		print('Error when fetching from OSS Index')
+		print(f"Http Status -> {response.status_code}")
+		print(f"Http Body -> {response.json()}")
+
---
+++ tests/test_ossindex_fetcher.py
@@ -0,0 +1,101 @@
+import json
+from gradle_bodyguard.ossindex_fetcher import OSSIndexFetcher
+
+FAKE_API='/fake/api/component-report'
+
+def test_fetched_with_success_no_vulnerabilities(httpserver):
+	
+	# Given
+	raw = '''
+	[
+	  {
+	    "coordinates": "pkg:maven/com.squareup.okhttp3/okhttp@4.5.0",
+	    "vulnerabilities": []
+	  },
+	  {
+	    "coordinates": "pkg:maven/com.squareup.retrofit2/retrofit@2.7.0",
+	    "vulnerabilities": []
+	  }
+	]
+	'''
+
+	httpserver.expect_request(FAKE_API).respond_with_json(json.loads(raw))
+	fetcher = OSSIndexFetcher(httpserver.url_for(FAKE_API))
+
+	# When
+	found = fetcher.fetch([])
+
+	# Then
+	expected = {}
+	assert found == expected
+
+def test_fetched_with_success_found_vulnerabilities(httpserver):
+	
+	# Given
+	raw = '''
+	[
+	  {
+	    "coordinates": "pkg:maven/com.squareup.okhttp3/okhttp@3.1.2",
+	    "vulnerabilities": [
+	      { "cve": "CVE-2018-20200" }
+	    ]
+	  },
+	  {
+	    "coordinates": "pkg:maven/com.squareup.retrofit2/retrofit@2.2.0",
+	    "vulnerabilities": []
+	  }
+	]
+	'''
+
+	httpserver.expect_request(FAKE_API).respond_with_json(json.loads(raw))
+	fetcher = OSSIndexFetcher(httpserver.url_for(FAKE_API))
+
+	# When
+	found = fetcher.fetch([])
+
+	# Then
+	expected = {
+		'pkg:maven/com.squareup.okhttp3/okhttp@3.1.2':'CVE-2018-20200'
+	}
+
+	assert found == expected
+
+def test_fetch_error_badrequest(httpserver):
+	
+	# Given
+	raw = '''
+	{
+	  "code": 400,
+	  "message": "Missing coordinates version"
+	}
+	'''
+
+	httpserver.expect_request(FAKE_API).respond_with_json(json.loads(raw), status = 400)
+	fetcher = OSSIndexFetcher(httpserver.url_for(FAKE_API))
+
+	# When
+	found = fetcher.fetch([])
+
+	# Then
+	expected = {}
+	assert found == expected
+
+def test_fetch_error_internal_server(httpserver):
+	
+	# Given
+	raw = '''
+	{
+	  "code": 503,
+	  "message": "Internal Server Error"
+	}
+	'''
+
+	httpserver.expect_request(FAKE_API).respond_with_json(json.loads(raw), status = 503)
+	fetcher = OSSIndexFetcher(httpserver.url_for(FAKE_API))
+
+	# When
+	found = fetcher.fetch([])
+
+	# Then
+	expected = {}
+	assert found == expected

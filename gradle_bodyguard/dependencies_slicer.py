f89e30a138c296e6bee71cebaeb38d549f5b47d1
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-06-01 22:17:47 +0200

    Added new implementation for dependencies slicer

---
+++ gradle_bodyguard/dependencies_slicer.py
@@ -0,0 +1,21 @@
+# dependencies_slicer.py
+
+def slice(target, chunk_size):
+	sliced = []
+	start = 0
+	end = len(target)
+	done = False
+
+	while not done and end - start != 0:
+		if end - start < chunk_size:
+			print(target[start:end])
+			sliced.append(target[start:end])
+			done = True
+		else :
+			cutpoint = start + chunk_size
+			print(target[start:cutpoint])
+			sliced.append(target[start:cutpoint])
+			start = cutpoint
+
+	return sliced
+
---
+++ tests/test_dependencies_slicer.py
@@ -0,0 +1,65 @@
+from gradle_bodyguard import dependencies_slicer
+
+def test_slicing_empty_list():
+	
+	# Given
+	target = []
+	chunck_size = 2
+
+	# When
+	sliced = dependencies_slicer.slice(target, chunck_size)
+
+	# Then
+	assert len(sliced) == 0
+
+def test_slicing_single_chunk():
+	
+	# Given
+	target = [1, 2, 3, 4, 5]
+	chunck_size = 5
+
+	# When
+	sliced = dependencies_slicer.slice(target, chunck_size)
+
+	# Then
+	expected = [[1, 2, 3, 4, 5]]
+	assert sliced == expected
+
+def test_slicing_fit_to_chunk():
+	
+	# Given
+	target = [1, 2, 3, 4, 5]
+	chunck_size = 3
+
+	# When
+	sliced = dependencies_slicer.slice(target, chunck_size)
+
+	# Then
+	expected = [[1, 2, 3], [4, 5]]
+	assert sliced == expected
+
+def test_slicing_chunk_size_overflow():
+	
+	# Given
+	target = [1, 2, 3, 4, 5]
+	chunck_size = 6
+
+	# When
+	sliced = dependencies_slicer.slice(target, chunck_size)
+
+	# Then
+	expected = [[1, 2, 3, 4, 5]]
+	assert sliced == expected
+
+def test_slicing_several_chunks():
+	
+	# Given
+	target = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	chunck_size = 4
+
+	# When
+	sliced = dependencies_slicer.slice(target, chunck_size)
+
+	# Then
+	expected = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10]]
+	assert sliced == expected

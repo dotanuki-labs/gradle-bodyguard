97c68ff1d7f0ae6bdbc52a4f03681715c9e63dc6
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-06-01 20:21:36 +0200

    Fixing invalid constructor

--- gradle_bodyguard/gradle_runner.py
+++ gradle_bodyguard/gradle_runner.py
@@ -2,12 +2,12 @@
 
 import subprocess
 
-class GradleTaskRunner(gradlew):
+class GradleTaskRunner:
 	def __init__(self, gradlew):
 		self.gradlew = gradlew
 		
 	def execute(self, task):
-		params = [gradlew, task]
+		params = [self.gradlew, task]
 		child_process = subprocess.Popen(params, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		stdout, stderr = child_process.communicate()
 		return f"{stdout}"

877dc3069ac31c76143d9472853f36e631014ee1
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2020-06-01 19:59:03 +0200

    Added utility to locate gradlew script in target project

---
+++ gradle_bodyguard/gradlew_locator.py
@@ -0,0 +1,13 @@
+# gradlew_locator.py
+
+import os
+import sys
+
+def locate(project):
+	gradlew = f"{project}/gradlew"
+	if os.path.isfile(gradlew):
+		os.chdir(project)
+		return gradlew
+	else:
+		print(f"💩 Error : gradlew script not found at {project}")
+		sys.exit(1)
